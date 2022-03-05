817cdb3a58b848d4a48622fd6d9ae089ab8b68cd
Kaloyan Daskalov <kdaskalov78@gmail.com>
2022-03-06 00:09:18 +0200

    add mysql connection

---
+++ src/config/database.ts
@@ -0,0 +1,24 @@
+import * as mysql from 'mysql2/promise'
+import { Config } from './index.js'
+
+// Connections
+export default async ({HOST, USER,PASSWORD, DATABASE}: Config) => {
+    let connection = await mysql.createConnection({
+        host: HOST,
+        user: USER,
+        password: PASSWORD,
+        database: DATABASE
+    });
+
+    connection.connect()
+        .then(() => connection.query<mysql.RowDataPacket[]>('SELECT * FROM EMPLOYEE'))
+        .then(([rows, fields]) => {
+            console.table(rows);
+        });
+
+    connection.connect()
+        .then(() => connection.execute<mysql.RowDataPacket[]>('SELECT 1 + 1 AS solution'))
+        .then(([rows, fields]) => {
+            console.log('The solution is: ', rows[0]['solution']);
+        });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import config from './config/index.js'
-import express, {Request, Response} from 'express'
+import express, {Request, Response, Application} from 'express'
+import database from './config/database.js'
 
-const app = express()
+const app:Application = express()
 
-app.get('', (rec: Request, res: Response) => {
+app.get('', async (rec: Request, res: Response) => {
     res.status(200).send('Node, Express, Typescript')
+    await database(config)
 })
 
 app.listen(config.PORT, () => {
