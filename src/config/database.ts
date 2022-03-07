65e99a5c0494bf6e0d952627b6a0311464a00bce
Kaloyan Daskalov <kdaskalov78@gmail.com>
2022-03-07 16:52:31 +0200

    mysql service

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,23 +2,24 @@ import * as mysql from 'mysql2/promise'
 import { Config } from './index.js'
 
 // Connections
-export default async ({HOST, USER,PASSWORD, DATABASE}: Config) => {
-    let connection = await mysql.createConnection({
+export default async ({HOST, USER,PASSWORD, DATABASE}: Config): Promise<mysql.Connection> => {
+    const connection = await mysql.createConnection({
         host: HOST,
         user: USER,
         password: PASSWORD,
         database: DATABASE
     });
 
-    connection.connect()
-        .then(() => connection.query<mysql.RowDataPacket[]>('SELECT * FROM EMPLOYEE'))
-        .then(([rows, fields]) => {
-            console.table(rows);
-        });
+    return connection;
+    // connection.connect()
+    //     .then(() => connection.query<mysql.RowDataPacket[]>('SELECT * FROM EMPLOYEE'))
+    //     .then(([rows, fields]) => {
+    //         console.table(rows);
+    //     });
 
-    connection.connect()
-        .then(() => connection.execute<mysql.RowDataPacket[]>('SELECT 1 + 1 AS solution'))
-        .then(([rows, fields]) => {
-            console.log('The solution is: ', rows[0]['solution']);
-        });
+    // connection.connect()
+    //     .then(() => connection.execute<mysql.RowDataPacket[]>('SELECT 1 + 1 AS solution'))
+    //     .then(([rows, fields]) => {
+    //         console.log('The solution is: ', rows[0]['solution']);
+    //     });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import config from './config/index.js'
 import express, {Request, Response, Application} from 'express'
 import database from './config/database.js'
+import { RowDataPacket } from 'mysql2'
 
 const app:Application = express()
 
+app.use(express.json())
+
 app.get('', async (rec: Request, res: Response) => {
-    res.status(200).send('Node, Express, Typescript')
-    await database(config)
+    const db = await database(config)
+    db.connect()
+        .then(() => db.query<RowDataPacket[]>('SELECT * FROM EMPLOYEE'))
+        .then(([rows, fields]) => {
+            res.status(200).json(rows)
+        });
+    // db.execute('SELECT * FROM EMPLOYEE').then(data => {
+    //     console.log(data);
+    // })
+
 })
 
 app.listen(config.PORT, () => {
